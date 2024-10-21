6b27c68b85164ba6d95a3064bc5dc173f6076fe4
eufouria <vietkhoa137@gmail.com>
2024-10-21 16:35:00 -0700

    update to crawl all programs

--- verra_scraper.py
+++ verra_scraper.py
@@ -4,8 +4,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from concurrent.futures import ThreadPoolExecutor
+import pandas as pd
+
 import os
 import requests
+import sys
+
+
+def get_project_ids(program, data_folder):
+    program_data_files = {
+        'CCB': ['ccbvcus.csv', 'allprojects.csv'],
+        'PWRP': ['plasticcredits.csv', 'allprojects.csv'],
+        'VCS': ['vcus.csv', 'allprojects.csv'],
+        'SDVISTA': ['sdvistavcus.csv', 'allprojects.csv'],
+        'CA_OPR': ['complianceprojects.csv']
+    }
+    proj_ids = []
+    for file in program_data_files.get(program):
+        df_00 = pd.read_csv(os.path.join(data_folder, program, file))
+        proj_ids += list(df_00['ID'].astype(str))
+    return list(set(proj_ids))
+
 
 def download_pdf(pdf_url, file_name, folder_name, proj_id):
     try:
@@ -18,7 +37,7 @@ def download_pdf(pdf_url, file_name, folder_name, proj_id):
     except requests.exceptions.RequestException as e:
         print(f"Error downloading {file_name}: {e}")
 
-def download_projects(proj_ids, parent_folder):
+def download_projects(parent_folder, program, proj_ids):
     # Set up WebDriver
     chrome_driver_path = '/Users/khoa/Downloads/chromedriver-mac-arm64/chromedriver'
     service = Service(executable_path=chrome_driver_path)
@@ -26,30 +45,61 @@ def download_projects(proj_ids, parent_folder):
 
     # Process each project
     for proj_id in proj_ids:
-        driver.get(f'https://registry.verra.org/app/projectDetail/VCS/{proj_id}')
+        driver.get(f'https://registry.verra.org/app/projectDetail/{program}/{proj_id}')
         
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "card-header")))
 
         # Verra documentation sections
-        sections = {
-            "VCS PIPELINE DOCUMENTS": "pipeline",
-            "VCS REGISTRATION DOCUMENTS": "registration",
-            "VCS ISSUANCE DOCUMENTS": "issuance",
-            "VCS OTHER DOCUMENTS": "other"
+        program_sections = {
+                "VCS" : {
+                    "VCS PIPELINE DOCUMENTS": "pipeline",
+                    "VCS REGISTRATION DOCUMENTS": "registration",
+                    "VCS ISSUANCE DOCUMENTS": "issuance",
+                    "VCS OTHER DOCUMENTS": "other"
+                },
+                
+                "PWRP" : {
+                    "PWRP PIPELINE DOCUMENTS": "pipeline",
+                    "PWRP REGISTRATION DOCUMENTS": "registration",
+                    "PWRP VERIFICATION DOCUMENTS": "verification",
+                    "PWRP OTHER DOCUMENTS": "other"
+                },
+                
+                "CCB" : {
+                    "CCB VALIDATION DOCUMENTS": "validation",
+                    "CCB VERIFICATION DOCUMENTS": "verification",
+                    "CCB OTHER DOCUMENTS": "other"
+                },
+
+                "SDVISTA" : {
+                    "SD VISTA OTHER DOCUMENTS": "other",
+                    "SD VISTA VERIFICATION DOCUMENTS": "verification",
+                    "SD VISTA REGISTRATION DOCUMENTS": "registration",
+                    "SD VISTA PIPELINE LISTING DOCUMENTS": "pipeline"
+                },
+
+                "CA_OPR" : {
+                    "COMPLIANCE LISTING DOCUMENTS": "listing",
+                    "COMPLIANCE ISSUANCE DOCUMENTS": "issuance",
+                    "COMPLIANCE OTHER DOCUMENTS": "other",
+                    "COMPLIANCE REGISTRATION DOCUMENTS": "registration"
+                }
         }
 
         # Create directories for each section
-        proj_path = os.path.join(parent_folder, proj_id)
+        proj_path = os.path.join(parent_folder, program.upper(), proj_id)
         os.makedirs(proj_path, exist_ok=True)
-        for folder in sections.values():
+        for folder in program_sections.get(program).values():
             os.makedirs(os.path.join(proj_path, folder), exist_ok=True)
 
         # Find section headers and process each section
         section_headers = driver.find_elements(By.XPATH, "//div[@class='card-header bg-primary']")
         for header in section_headers:
             section_title = header.text.strip()
-            folder_name = sections.get(section_title, "other")
-            
+            if section_title not in program_sections.get(program):
+                print(f"Skipping section '{section_title}'")
+                continue
+            folder_name = program_sections.get(program).get(section_title, "other")
             try:
                 card_body = header.find_element(By.XPATH, "./following-sibling::div[contains(@class,'card-body')]")
                 pdf_links = card_body.find_elements(By.XPATH, ".//tbody//tr//td//a")
@@ -74,7 +124,6 @@ def download_projects(proj_ids, parent_folder):
 if __name__ == "__main__":
     parent_folder = "verra"
     os.makedirs(parent_folder, exist_ok=True)
-    import pandas as pd
-    df = pd.read_csv('allprojects.csv')
-    proj_ids = list(df['ID'].astype(str))
-    download_projects(proj_ids, parent_folder)
+    program_arg = sys.argv[1].upper()
+    proj_ids = get_project_ids(program_arg, 'program_data')
+    download_projects(parent_folder, program_arg, ['2342'])
